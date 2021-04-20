6604396dd0d58acf1c9b8e56b5c1209bc86bbb06
dogota_n <dogota_n@etna-alternance.net>
2021-04-20 23:58:27 +0200

    first working version of parsing map

--- sdl/include/map.h
+++ sdl/include/map.h
@@ -1,5 +1,17 @@
+#include <stdlib.h>
+
 #define MAP_WIDTH 10
 #define MAP_HEIGHT 10
 
-int size_map(char *file_name, int *map_width, int *map_height);
+// player struct
+struct player {
+    float x;
+    float y;
+    float height;
+    float width;
+} player;
+
+void print_map(char** map, int height, int width);
+char** load_map(char *file_name, int height, int width);
+int size_map(char *dump, int *map_height, int *map_width);
 int map_syntax(char *file_name);
--- sdl/src/map.c
+++ sdl/src/map.c
@@ -2,7 +2,43 @@
 
 #include "../include/map.h"
 
-int size_map(char *file_name, int *map_width, int *map_height)
+void print_map(char** map, int height, int width)
+{
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width + 1; j++) {
+            printf("%c", map[i][j]);
+        }
+    }
+}
+
+char** load_map(char *file_name, int height, int width)
+{
+    FILE *F;
+    int w = 0;
+
+    F = fopen(file_name, "r");
+
+    if(!F)
+        return NULL;
+
+    char **map_data = malloc(height * sizeof(char *));
+
+    char c = fgetc(F);
+
+    for(int h = 0; c != EOF && h < height; c = fgetc(F), h++) {
+        map_data[h] = malloc((width + 1) * sizeof(char));
+
+        for(w = 0; c != '\n' && w < width; c = fgetc(F), w++) {
+            map_data[h][w] = c;
+        }
+
+        map_data[h][w] = '\n';
+    }
+
+    return map_data;
+}
+
+int size_map(char *file_name, int *map_height, int *map_width)
 {
     int max_width, width, height = 0;
 
@@ -10,10 +46,8 @@ int size_map(char *file_name, int *map_width, int *map_height)
 
     F = fopen(file_name, "r");
 
-    if(!F) {
-        printf("The map file : %s doesnt exist.\n", file_name);
+    if(!F)
         return -1;
-    }
 
     for(char c = fgetc(F); c != EOF; c = fgetc(F), height++) {
         for(; c != EOF && c != '\n'; c = fgetc(F), width++);
@@ -23,8 +57,8 @@ int size_map(char *file_name, int *map_width, int *map_height)
         width = 0;
     }
 
-    *map_width = max_width;
     *map_height = height;
+    *map_width = max_width;
 
     return 0;
 }
@@ -32,7 +66,7 @@ int size_map(char *file_name, int *map_width, int *map_height)
 int map_syntax(char *file_name)
 {
     FILE *F;
-    int map_width, map_height;
+    int map_height, map_width;
 
     F = fopen(file_name, "r");
 
@@ -48,14 +82,18 @@ int map_syntax(char *file_name)
         }
     }
 
-    size_map(file_name, &map_width, &map_height);
+    size_map(file_name, &map_height, &map_width);
 
-    if(map_width != MAP_WIDTH && map_height != MAP_HEIGHT) {
-        printf("The map file doesnt correspond with the correct size : %s\n", file_name);
+    if(map_height != MAP_HEIGHT && map_width != MAP_WIDTH) {
+        printf("The map file have an invalid syntax : %s\n", file_name);
         return -1;
     }
 
+    char** map_data = load_map(file_name, map_height, map_width);
 
+    print_map(map_data, map_height, map_width);
+
+    //printf("map_height : %d - map_width : %d", map_height, map_width);
 
     return 0;
 }
